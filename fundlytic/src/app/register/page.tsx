f4d8da29be030b10f7e39e40dbbe4cd8db4e6de0
SGalindo831 <galindo.sabino831@gmail.com>
2025-10-16 16:48:23 -0700

    Added pages and navigation for pages

---
+++ fundlytic/src/app/register/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function GroupPage() {
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push("/");
+  };
+
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-4">Register Page</h1>
+      <p className="text-gray-700">Welcome to the Register!</p>
+      
+      <button onClick={goHome}
+      className="mt-2 bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700 transition"
+      >
+      home</button>
+    </div>
+  );
+}
